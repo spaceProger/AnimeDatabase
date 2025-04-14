7d870c59bfd5074868801e750be235c5ab3ad384
spacer <f-balykin@mail.ru>
2025-04-14 23:01:52 +0500

    ADD: mount "/static" url to "templates" directory

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from src.routers import include_routers
 
 
 app = FastAPI()
+
+app.mount('/static', StaticFiles(directory="templates"), name="static")
 
 include_routers(app)
