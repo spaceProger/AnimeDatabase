7bd6d5fd70fe6f5c38b193efab8a0aad605dc353
spacer <f-balykin@mail.ru>
2025-04-15 20:00:51 +0500

    ADD: pydantic-settings at src/config.py

--- src/config.py
+++ src/config.py
@@ -1,1 +1,43 @@
-PROJECT_NAME = "anime-database"
+from pydantic_settings import BaseSettings
+from pydantic_settings import SettingsConfigDict
+
+
+class DatabaseConfig(BaseSettings):
+    DB_USER: str
+    DB_NAME: str
+    DB_ECHO: bool
+    DB_HOST: str
+    DB_PORT: int
+    DB_PASSWORD: str
+
+    DEV_DB_FILEPATH: str
+
+    @property
+    def DB_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@\
+                {self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    @property
+    def DEV_DB_URL(self) -> str:
+        return f"sqlite+aiosqlite://{self.DEV_DB_FILEPATH}"
+
+
+class ServerConfig(BaseSettings):
+    APP_HOST: str
+    APP_PORT: int
+    APP_WORKERS: int
+
+
+class MainConfig(DatabaseConfig, ServerConfig, BaseSettings):
+    PROJECT_NAME = "anime-database"
+
+    RUNTYPE: str
+
+    model_config = SettingsConfigDict(
+            env_file=".env",
+            env_file_encoding="utf-8",
+    )
+
+
+main_config = MainConfig()
+
